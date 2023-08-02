938d0b1df24d6fc2cba7f788ceb31b6dab7392ff
Cristian Bontoiu <cbontoiu@gmail.com>
2023-08-02 12:38:13 +0100

    different tests

--- OneModuleCI/src/main/java/utilities/A.java
+++ OneModuleCI/src/main/java/utilities/A.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 class A implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> i + 1;
+        return i -> i + 3;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i + 5;
+        return i + 0;
     }
 }
