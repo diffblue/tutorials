511156ce1365f4cbfc729ee5063891a2246de4f7
Cristian Bontoiu <cbontoiu@gmail.com>
2023-08-04 14:32:49 +0100

    ultimate

--- OneModuleCI/src/main/java/utilities/A.java
+++ OneModuleCI/src/main/java/utilities/A.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 class A implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> i + 0;
+        return i -> i;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i + 0;
+        return i;
     }
 }
