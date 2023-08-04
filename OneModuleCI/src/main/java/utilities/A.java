dc898f3c8808ee39bf22b8d5627aba2231157f91
Cristian Bontoiu <cbontoiu@gmail.com>
2023-08-04 12:04:27 +0100

    more changes

--- OneModuleCI/src/main/java/utilities/A.java
+++ OneModuleCI/src/main/java/utilities/A.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 class A implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> i + 12;
+        return i -> i + 1;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i + 2;
+        return i + 1;
     }
 }
--- OneModuleCI/src/main/java/utilities/B.java
+++ OneModuleCI/src/main/java/utilities/B.java
@@ -5,11 +5,11 @@ import java.util.function.Function;
 class B implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> i * 15;
+        return i -> i * 2;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i * 20;
+        return i * 4;
     }
 }
