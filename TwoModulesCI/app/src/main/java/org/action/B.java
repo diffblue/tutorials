cc29f73b4d2ec57203ed57645446e5b4455e15e2
Cristian Bontoiu <cbontoiu@gmail.com>
2023-08-10 17:25:45 +0100

    final changes

--- TwoModulesCI/app/src/main/java/org/action/B.java
+++ TwoModulesCI/app/src/main/java/org/action/B.java
@@ -7,11 +7,11 @@ import java.util.function.Function;
 public class B implements I {
         @Override
         public Function<Integer, Integer> getFunction() {
-            return i -> 52 * i;
+            return i -> 2 * i;
         }
 
         @Override
         public Integer operation(Integer i) {
-            return i + 52;
+            return i + 2;
         }
     }
--- TwoModulesCI/common/src/main/java/org/utils/A.java
+++ TwoModulesCI/common/src/main/java/org/utils/A.java
@@ -6,11 +6,11 @@ import java.util.function.Function;
 class A implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> 54 * i;
+        return i -> 4 * i;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i + 54;
+        return i + 4;
     }
 }
