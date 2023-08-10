f7ce6de75d45846ac5103855677a87fb144bc058
Cristian Bontoiu <cbontoiu@gmail.com>
2023-08-10 18:31:50 +0100

    demo two-modules

--- TwoModulesCI/common/src/main/java/org/utils/A.java
+++ TwoModulesCI/common/src/main/java/org/utils/A.java
@@ -6,11 +6,11 @@ import java.util.function.Function;
 class A implements I {
     @Override
     public Function<Integer, Integer> getFunction() {
-        return i -> 14 * i;
+        return i -> 4 * i;
     }
 
     @Override
     public Integer operation(Integer i) {
-        return i + 14;
+        return i + 4;
     }
 }
